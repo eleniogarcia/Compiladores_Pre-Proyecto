2f6610aaebe399b17bf670fcbacb7a4e764bea2e
yaguer11 <yaguer590@gmail.com>
2025-10-27 11:06:24 -0300

    Modificaciones parcer

--- out/production/02 - ejemplo parser/program.asm
+++ out/production/02 - ejemplo parser/program.asm
@@ -1,21 +1,36 @@
 main:
         pushq   %rbp
         movq    %rsp, %rbp
-        subq    $32, %rsp
-        movq    $10, %rax
-        movq    %rax, -8(%rbp)
-        movq    $5, %rax
+        subq    $24, %rsp
+        movq    -8(%rbp), %rax
+        pushq   %rax
+        movq    $3, %rax
+        pushq   %rax
+        movq    $2, %rax
+        popq    %rcx
+        imulq   %rcx, %rax
+        pushq   %rax
+        movq    -24(%rbp), %rax
+        popq    %rcx
+        imulq   %rcx, %rax
+        popq    %rcx
+        addq    %rcx, %rax
         movq    %rax, -16(%rbp)
         movq    -8(%rbp), %rax
-        movq    %rax, %rcx
-        movq    -16(%rbp), %rax
-        addq    %rcx, %rax
-        movq    %rax, -24(%rbp)
-        movq    -8(%rbp), %rax
-        movq    %rax, %rcx
-        movq    -16(%rbp), %rax
-        imulq   %rcx, %rax
-        movq    %rax, -32(%rbp)
+        pushq   %rax
+        movq    -24(%rbp), %rax
+        popq    %rcx
+        cmpq    $0, %rax
+        je      L_else_0
+        movq    $1, %rax
+        leave
+        ret
+        jmp     L_end_1
+        L_else_0:
+        movq    $2, %rax
+        leave
+        ret
+        L_end_1:
         movq    $0, %rax
         leave
         ret
